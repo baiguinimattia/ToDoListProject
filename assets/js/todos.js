7e3fda4c94ce232a6f32896ca90e70a3e464c431
Baiguini Mattia <mattia.baiguini@gmail.com>
2018-07-07 15:16:36 +0300

    functionality
    Todo list application in javascript.

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+// $("input").keypress(function(event){
+//     if ( event.which === 13){
+//             console.log("tttt");
+//             $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+//             $("input").val("");
+//     }
+// });
+
+$("input").on("keypress" , function(event){
+    if ( event.which === 13){
+        console.log("tttt");
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $("input").val("");
+}
+});
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click" , "span" , function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
